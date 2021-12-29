33eccafc913e50d3daf5ac8f6b987bf03a004d08
xzcube <1482240153@qq.com>
2021-12-29 11:09:16 +0800

    gin_demo

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default() // 返回默认的路由引擎
+
+	r.GET("/hello", sayHello) // 指定用户使用GET请求 /hello 时，用sayHello函数
+	r.Run(":9090")
+}
+
+func sayHello(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "hello golang",
+	})
+
+}
